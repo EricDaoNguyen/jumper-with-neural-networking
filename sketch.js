4daed9726ccd8a39b8fd8d1f06128717ee5a876b
EricDaoNguyen <EricDaoNguyen@Gmail.com>
2021-09-30 19:53:06 -0400

    Added canvas and jumper.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
+// Canvas
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(750, 750);
+  background(0);
 }
 
+// Renderer
 function draw() {
-  background(220);
+  jumper = new Jumper()
+  jumper.show()
+}
+
+// Jumper
+function Jumper() {
+  // Starting position
+  this.x = 30
+  this.y = height / 2
+
+  // Jumper sprite
+  this.show = function() {
+    fill(255)
+    ellipse(this.x, this.y, 15, 15)
+  }
 }
