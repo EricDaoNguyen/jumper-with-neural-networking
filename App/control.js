38f492209c04918f8dd2ad5484d821b09d1c2c75
EricDaoNguyen <EricDaoNguyen@Gmail.com>
2021-10-01 14:15:52 -0400

    Added resizing canvas options and instructions.

--- App/control.js
+++ App/control.js
@@ -3,4 +3,16 @@ function keyPressed() {
     jumper.up()
     if(isOver) { reset() }
   }
+  if(key === `1`) {
+    resizeCanvas(500, 500)
+    reset()
+  }
+  if(key === `2`) {
+    resizeCanvas(1000, 1000)
+    reset()
+  }
+  if(key === `3`) {
+    resizeCanvas(1500, 1500)
+    reset()
+  }
 }
