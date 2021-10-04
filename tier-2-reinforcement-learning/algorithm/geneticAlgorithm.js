0c97ae40b5122533db785b293bb540c11eba53bc
EricDaoNguyen <EricDaoNguyen@Gmail.com>
2021-10-04 05:19:13 -0400

    Fitness properly calculates.

--- tier-2-reinforcement-learning/algorithm/geneticAlgorithm.js
+++ tier-2-reinforcement-learning/algorithm/geneticAlgorithm.js
@@ -4,19 +4,26 @@ function nextGeneration() {
   calculateFitness()
   for(let i = 0; i < totalJumpers; i++) { jumpers[i] = pickJumper() }
   savedJumpers = []
-  console.log(`New generation loaded.`)
+  console.log(`New generation loaded: ${jumpers}`)
 }
 
 // Each fitness value is a probability for the next generation
 function calculateFitness() {
   let result = 0
-  for(let jumper of jumpers) { result += jumper.score }
-  for(let jumper of jumpers) { jumper.fitness = jumper.score / result }
+  for(let jumper of savedJumpers) { result += jumper.score }
+  for(let jumper of savedJumpers) { jumper.fitness = jumper.score / result }
 }
 
 // Simplified crossover, instead of combining data of two parents, we're just making a copy
-function pickJumper() {
-  let jumper = random(savedJumpers)
+function pickJumper(jumpers) {
+  let index = 0
+  let rand = random(1)
+  while(rand > 0) {
+    rand = rand - savedJumpers[index].fitness
+    index++
+  }
+  index--
+  let jumper = savedJumpers[index]
   let child = new Jumper(jumper.brain)
   child.mutate()
   return child
