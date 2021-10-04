f2b18b05348a844aca4973356c2168c0231c7aa8
EricDaoNguyen <EricDaoNguyen@Gmail.com>
2021-10-04 07:33:25 -0400

    Added copy function.

--- tier-3-tensorflow/neuralNetwork/neuralNetwork.js
+++ tier-3-tensorflow/neuralNetwork/neuralNetwork.js
@@ -1,27 +1,37 @@
+// Documentation: https://www.tensorflow.org/js
+
 class NeuralNetwork {
-  constructor(a, b, c) {
-    this.input_nodes = a
-    this.hidden_nodes = b
-    this.output_nodes = c
-    this.createModel()
+  constructor(a, b, c, d) {
+    if(a instanceof tf.Sequential) {
+      this.model = a
+      this.input_nodes = b
+      this.hidden_nodes = c
+      this.output_nodes = d
+    } else {
+      this.input_nodes = a
+      this.hidden_nodes = b
+      this.output_nodes = c
+      this.model = this.createModel()
+    }
   }
 
-  // Documentation: https://www.tensorflow.org/js
   createModel() {
-    this.model = tf.sequential()
+    const model = tf.sequential()
 
     const hidden = tf.layers.dense({
       units: this.hidden_nodes,
       inputShape: [this.input_nodes],
       activation: 'sigmoid'
     })
-    this.model.add(hidden)
+    model.add(hidden)
 
     const output = tf.layers.dense({
       units: this.output_nodes,
       activation: 'softmax'
     })
-    this.model.add(output)
+    model.add(output)
+
+    return model
   }
 
   // Convert inputs to tensor and return outputs
@@ -30,5 +40,13 @@ class NeuralNetwork {
     const y = this.model.predict(x)
     const outputs = y.dataSync()
     return outputs
+  }
+
+  // Copies architecture with the weights
+  copy() {
+    const modelCopy = this.createModel()
+    const weights = this.model.getWeights()
+    modelCopy.setWeights(weights)
+    return new NeuralNetwork(modelCopy, this.input_nodes, this.hidden_nodes, this.output_nodes)
   }
 }
