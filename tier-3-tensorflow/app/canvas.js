06860eb57ec78f060cbad2b10a80e69501c45c37
EricDaoNguyen <EricDaoNguyen@Gmail.com>
2021-10-04 05:54:43 -0400

    Added tier 3 folder.

---
+++ tier-3-tensorflow/app/canvas.js
@@ -0,0 +1,67 @@
+let jumpers = []
+let jumperSprite
+const totaljumpers = 500
+let savedJumpers = []
+let blockers = []
+let imageBackground
+let backgroundMusic
+let counter = 0
+let cycleSlider
+
+// Preloads
+function preload() {
+  imageBackground = loadImage(`../assets/background.jpg`)
+  jumperSprite = loadImage(`../assets/goat.png`)
+  backgroundMusic = loadSound(`../assets/backgroundMusic.mp3`)
+}
+
+// Canvas
+function setup() {
+  createCanvas(1000, 1000)
+  cycleSlider = createSlider(1, 100, 1)
+  backgroundMusic.play()
+  backgroundMusic.loop()
+  for(let i = 0; i < totaljumpers; i++) { jumpers[i] = new Jumper() }
+}
+
+// Renderer
+function draw() {
+  image(imageBackground, 0, 0)
+  for(let jumper of jumpers) { jumper.show() }
+  for(let blocker of blockers) { blocker.show() }
+
+  for(let cycle = 0; cycle < cycleSlider.value(); cycle++) {
+    // New set of blockers every 80 frames per second
+    if(counter % 80 === 0) { blockers.push(new Blocker()) }
+    counter++
+
+    // New set of jumpers
+    for(let jumper of jumpers) {
+      jumper.update()
+      jumper.think(blockers)
+    }
+
+    // Reset with the next generation when all jumpers are eliminated
+    if(jumpers.length === 0) {
+      counter = 0
+      nextGeneration()
+      blockers = []
+    }
+
+    for(let i = blockers.length - 1; i >= 0; i--) {
+      blockers[i].show()
+      blockers[i].update()
+      // Remove blockers as soon as they are off left of canvas
+      if(blockers[i].offCanvas()) {
+        blockers.splice(i, 1)
+        console.log(`Current length of blockers array: ${blockers.length}`)
+      }
+      if(blockers[i].hit(jumpers)) { gameOver() }
+
+      // Eliminate jumpers that hit a blocker
+      for(let j = jumpers.length - 1; j >= 0; j--) {
+        if(blockers[i].hit(jumpers[j])) { savedJumpers.push(jumpers.splice(j, 1)[0]) }
+      }
+    }
+  }
+}
---
+++ tier-3-tensorflow/app/jumper.js
@@ -0,0 +1,78 @@
+// Jumper
+class Jumper {
+  constructor(brain) {
+    // Starting position
+    this.x = 128
+    this.y = height / 2
+
+    // Push and pull force
+    this.gravity = 0.5
+    this.lift = 10.5
+    this.velocity = -8.5
+
+    // Jumper sprite
+    this.icon = jumperSprite
+    this.height = 128
+    this.width = 128
+
+    // Jumper brain
+    this.brain = new NeuralNetwork(4, 4, 2)
+
+    // Each jumper's score increases the longer it stays alive
+    this.score = 0
+    this.fitness = 0
+
+    // Create a copy if jumper's brain exist
+    if(brain) { this.brain = brain.copy() }
+    else {this.brain = new NeuralNetwork(4, 4, 2)}
+  }
+
+  // Show jumper sprite
+  show() { image(this.icon, this.x - this.width / 2, this.y - this.height / 2, this.width, this.height) }
+
+  // Jumper jumps
+  up() { this.velocity -= this.lift + 3 }
+
+  update() {
+    // Jumper falls
+    this.velocity += this.gravity
+    this.y += this.velocity
+
+    // A jumper's score increments by 1 per blocker passed
+    this.score++
+
+    // Prevents jumper from going off top and bottom of canvas
+    if(this.y < 0) {
+      this.y = 0
+      this.velocity = 0
+    }
+    if(this.y > height) {
+      this.y = height
+      this.velocity = 0
+    }
+  }
+
+  // Allows jumper to jump on it's own
+  think(blockers) {
+    // Finds closest blocker
+    let closestBlocker = null
+    let closestDistance = Infinity
+    for(let i = 0; i < blockers.length; i++) {
+      let distance = blockers[i].x - this.x
+      if(distance < closestDistance && distance > 0) {
+        closestBlocker = blockers[i]
+        closestDistance = distance
+      }
+    }
+    let input = []
+    input[0] = this.y / height
+    input[1] = closestBlocker.top / height
+    input[2] = closestBlocker.bottom / height
+    input[3] = closestBlocker.x / width
+    let output = this.brain.predict(input)
+    if(output[0] > output[1]) { this.up() }
+  }
+
+  // Mutate child's data based on calculated fitness
+  mutate() { this.brain.mutate(0.1) }
+}
